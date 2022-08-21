56f1d75b286bbcb873fd490e84d32c4d1b4c09d6
Nothnbutthefacts <43274071+Nothnbutthefacts@users.noreply.github.com>
2022-08-21 14:12:24 -0600

    Update Types.hs
    Cleaned up some of the spacing; removed comment from 'fdbck'

--- src/Types.hs
+++ src/Types.hs
@@ -2,16 +2,16 @@ module Types where
 
 data Color = Green | Blue | Red | Yellow | Pink | Brown | Orange | Purple | White deriving (Show, Eq, Read, Enum, Bounded)
 
-type Alert = String
-type Guess = [Color]
-type Round = Int
+type Alert  = String
+type Guess  = [Color]
+type Round  = Int
 type Rounds = Int
 
-type Name = String
+type Name   = String
 type Master = String
 data Player = Player
-  { name :: !String
-  , score :: !Int
+  { name        :: !String
+  , score       :: !Int
   , timesMaster :: !Int
   } deriving Show
 
@@ -24,15 +24,15 @@ data PrepStatus = Prepared | ErrorInPrep
 
 data Secret = Secret
   { numberSlots :: !Int
-  , secretCode :: ![Color]
+  , secretCode  :: ![Color]
   } deriving Show
 
 makeSecret :: [Color] -> Secret
 makeSecret colors = Secret (length colors) colors
 
 data Play = Play
-  { guess :: !Guess
-   --, fdbck :: !Feedback
+  { guess  :: !Guess
+  , fdbck  :: !Feedback
   , player :: !Player
   }
 
@@ -41,13 +41,13 @@ makeGame p m s r = Game r 0 [] [] m p s Continue
   
 data Game = Game
   { roundsRemaining :: !Int
-  , currentRound :: !Int
-  , guessesHistory :: ![Play]
-  , fdbckHistory :: ![Feedback]
-  , master :: !Master
-  , players :: ![Player]
-  , secret :: !Secret
-  , status :: !GameStatus
+  , currentRound    :: !Int
+  , guessesHistory  :: ![Play]
+  , fdbckHistory    :: ![Feedback]
+  , master          :: !Master
+  , players         :: ![Player]
+  , secret          :: !Secret
+  , status          :: !GameStatus
   }
            
 data Feedback = 
